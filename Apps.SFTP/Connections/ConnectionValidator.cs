fac7e98456393cd035db1eaccdf3627fe0b09708
sokolenkoolha <olha.sokolenko@blackbird.io>
2023-10-27 19:05:21 +0300

    preparations for the artifactory

---
+++ Apps.SFTP/Connections/ConnectionValidator.cs
@@ -0,0 +1,26 @@
+﻿using Blackbird.Applications.Sdk.Common.Authentication;
+using Blackbird.Applications.Sdk.Common.Connections;
+
+namespace Apps.SFTP.Connections;
+
+public class ConnectionValidator : IConnectionValidator
+{
+    public async ValueTask<ConnectionValidationResponse> ValidateConnection(
+        IEnumerable<AuthenticationCredentialsProvider> authProviders, CancellationToken cancellationToken)
+    {
+        using var client = new BlackbirdSftpClient(authProviders);
+
+        if (client.IsConnected)
+            return new ConnectionValidationResponse
+            {
+                IsValid = true
+            };
+        
+        return new ConnectionValidationResponse
+        {
+            IsValid = true,
+            Message = "Failed to connect. Please check your connection parameters."
+        };
+
+    }
+}

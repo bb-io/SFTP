2ff93e40e31edf5dd37b353ca0d96b69fb143e22
bZverok <137277669+bZverok@users.noreply.github.com>
2024-04-10 12:48:01 +0300

    added categories

--- Apps.SFTP/SftpApplication.cs
+++ Apps.SFTP/SftpApplication.cs
@@ -1,9 +1,17 @@
 ﻿using Blackbird.Applications.Sdk.Common;
+using Blackbird.Applications.Sdk.Common.Metadata;
 
 namespace Apps.SFTP;
 
-public class SftpApplication : IApplication
+public class SftpApplication : IApplication, ICategoryProvider
 {
+    
+    public IEnumerable<ApplicationCategory> Categories
+    {
+        get => [ApplicationCategory.FileManagementAndStorage];
+        set { }
+    }
+    
     public string Name
     {
         get => "SFTP";

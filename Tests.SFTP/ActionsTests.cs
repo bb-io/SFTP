9485f949963d7b555825643a67159bf6019c6088
Alex Terehov <terales@users.noreply.github.com>
2025-10-08 19:21:17 -0230

    Maintenance release: Blackbird SDK dependency updates (no action required)

--- Tests.SFTP/ActionsTests.cs
+++ Tests.SFTP/ActionsTests.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Apps.SFTP;
 using Apps.SFTP.Models.Requests;
 using Blackbird.Applications.Sdk.Common.Files;
 using Tests.SFTP.Base;
